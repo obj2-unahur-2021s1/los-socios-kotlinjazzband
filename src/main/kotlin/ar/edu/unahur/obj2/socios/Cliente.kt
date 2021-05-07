adc9d57205754792e706d3e870cf9dc1380a11ff
jlfarias <jlfarias2009@gmail.com>
2021-05-07 08:17:55 -0300

    retornar mayor valor

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -15,13 +15,18 @@ class Cliente(val estadoDeAnimo: EstadoDeAnimo, val dineroQueTengo: DineroEnElBo
             else -> 0
         }
     }
+    fun maximoValor(valor1: Int, valor2:Int ): Int {
+        var maximo = valor1
+        if (valor2 >= valor1){ maximo = valor2 }
+        return maximo
+    }
 
     fun dejaPropina(barrioEnQueVive: Barrio): Int {
         return when (barrioEnQueVive) {
             lasRosas -> this.calculoPropinaPorEstadoDeAnimo(estadoDeAnimo) + 50
             lasLauchas -> this.calculoPropinaPorEstadoDeAnimo(estadoDeAnimo) / 2
             //barrioVerde tiene que dar 200 para arriba de propina
-            barrioVerde -> this.calculoPropinaPorEstadoDeAnimo(estadoDeAnimo)
+            barrioVerde -> this.maximoValor(calculoPropinaPorEstadoDeAnimo(estadoDeAnimo), 200)
             //Las torres no modifica su propina
             else -> this.calculoPropinaPorEstadoDeAnimo(estadoDeAnimo)
         }
